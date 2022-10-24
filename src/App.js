cb7c50724fd727ca2e46f726eebda794f14cc5a5
liuweibo <liuweibo@haier.com>
2022-10-24 09:48:51 +0800

    pref code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css'
 import { useState } from 'react'
 import { IntlProvider, FormattedMessage } from 'react-intl'
-import en from './content/compiled-locales/en.json'
-import zh from './content/compiled-locales/zh_CN.json'
+import en from './content/locales/en.json'
+import zh from './content/locales/zh_CN.json'
 function App () {
   const [msgs, setMsgs] = useState('en')
   return (
     <div className="App">
       <h1>测试国际化方案</h1>
-      <button onClick={() => setMsgs('zh')}>切换中文</button>
+      <button onClick={() => setMsgs('zh-CN')}>切换中文</button>
       <button onClick={() => setMsgs('en')}>切换英文</button>
       <br/>
       <br/>
       <br/>
-      <IntlProvider messages={msgs === 'zh' ? zh : en} locale="zh" defaultLocale="en">
+      <IntlProvider messages={msgs === 'zh-CN' ? zh : en} locale={msgs} defaultLocale="en">
         <p>
           <FormattedMessage
             id="myMessage2"
